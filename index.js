2f1c135a27db120c18b2143d4657ba631beaef9e
Tahsin Nurun <nuruntahsin@gmail.com>
2021-08-01 19:09:39 +0600

    working on animation

--- index.js
+++ index.js
@@ -3,41 +3,49 @@ var ham = document.querySelector(".ham_btn");
 var xIcon = document.querySelector(".x_icon");
 var menuIcon = document.querySelector(".ham_icon");
 
-ham.addEventListener("click",toggleMenu);
+ham.addEventListener("click", toggleMenu);
 
 menu.classList.contains("show_res_menu");
 
-function toggleMenu(){
-   
-    if(menu.classList.contains("show_res_menu")){
+function toggleMenu() {
+
+    if (menu.classList.contains("show_res_menu")) {
         menu.classList.remove("show_res_menu");
         xIcon.style.display = "none";
-        menuIcon.style.display="block";
-    }else{
+        menuIcon.style.display = "block";
+    } else {
         menu.classList.add("show_res_menu");
         xIcon.style.display = "block";
-        menuIcon.style.display="none";
+        menuIcon.style.display = "none";
     }
 }
 
 var menuLinks = document.querySelectorAll(".nav_tab_res");
-menuLinks.forEach(function(menuLink){
+menuLinks.forEach(function (menuLink) {
     menuLink.addEventListener("click", toggleMenu)
 })
 
 // fadeIn Animation
 
-var card = document.querySelectorAll(".card");
-// card.style.display = "block";
+function service_animation() {
 
+    // var visibleCards = document.getElementById("ser_cards");
+    // console.log(visibleCards);
+    // visibleCards.style.display = "block";
 
+    var card = document.querySelectorAll(".card");
+    // card.style.display = "block";
 
-// card.forEach(function(x){
-//     x.classList.add("fade_animation")
-// })
+    
 
-for(var i = 0; i < card.length; i++){
-    card[i].classList.add("fade_animation");
+    // card.forEach(function(x){
+    //     x.classList.add("fade_animation")
+    // })
+
+    for (var i = 0; i < card.length; i++) {
+        card[i].classList.add("fade_animation");
+    }
+
 }
 
 
@@ -58,7 +66,7 @@ for(var i = 0; i < card.length; i++){
 //         console.log("dataLoading inside settimeout task 2")
 //         callback();
 //     }, 2000);
-    
+
 //     // console.log("task2")
 // }
 
@@ -93,7 +101,7 @@ for(var i = 0; i < card.length; i++){
 //     }else{
 //         reject("bodmaish kothakar")
 //     }
-    
+
 // })
 
 // promise1.then(res=>{
